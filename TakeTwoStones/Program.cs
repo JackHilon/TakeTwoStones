b66421f1485857728353a8aea3c97508d913e268
markandmark1111@gmail.com <markandmark1111@gmail.com>
2020-06-20 00:52:26 +0200

    Add project files.

---
+++ TakeTwoStones/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace TakeTwoStones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Take Two Stones
+            // https://open.kattis.com/problems/twostones
+
+            int number = EnterNum();
+
+            string name = PlayerName(number);
+
+            Console.WriteLine(name);
+        }
+
+        private static string PlayerName(int num)
+        {
+            if (num % 2 == 0)
+                return "Bob";
+            else
+                return "Alice";
+        }
+        private static int EnterNum()
+        {
+            int a = 1;
+            string str = "";
+            str = Console.ReadLine();
+            try
+            {
+                a = int.Parse(str);
+                if (a < 1 || a > 10000000)
+                    throw new ArgumentException();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return EnterNum();
+            }
+            return a;
+        }
+    }
+}
